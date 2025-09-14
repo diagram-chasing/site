b7ccaf54e15bedb0a747e470ca602bd8da3b270c
Aman Bhargava <amanbhargava2001@gmail.com>
2025-09-14 16:10:05 +0530

    feat: responsive grids

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<h1
+	class="font-serif text-xl leading-tight font-black tracking-tighter text-foreground sm:text-2xl md:text-3xl lg:text-5xl xl:text-6xl"
+>
+	Support our work
+</h1>
